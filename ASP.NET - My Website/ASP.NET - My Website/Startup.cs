ffab5505f307ab04162b8f40573344178c9b7128
Hoang Huu Tin <fd19919@gmail.com>
2017-05-02 04:05:15 +0700

    Create Default ASP.NET application

---
+++ ASP.NET - My Website/ASP.NET - My Website/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET___My_Website.Startup))]
+namespace ASP.NET___My_Website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
